ba61a40f1dae150a1cc4dd65864891c4164ace08
Ashley <afletcher679@gmail.com>
2024-10-25 13:59:38 -0400

    Separate Utils and PassingProvider methods
    fix comma string on passingYards
    remove unused variable

--- app/src/main/java/com/example/sportspredictions/passingstats/PassingStatsProvider.kt
+++ app/src/main/java/com/example/sportspredictions/passingstats/PassingStatsProvider.kt
@@ -1,7 +1,9 @@
 package com.example.sportspredictions.passingstats
 
+import com.example.sportspredictions.shared.Utils.Companion.getStatsFromTable
+import com.example.sportspredictions.shared.Utils.Companion.removeCommaFromNumberString
+import com.example.sportspredictions.shared.Utils.Companion.splitNameFromPosition
 import com.example.sportspredictions.webscraping.WebScrapeUtils.Companion.getAllTables
-import com.example.sportspredictions.webscraping.WebScrapeUtils.Companion.getColumnsFromRow
 import com.example.sportspredictions.webscraping.WebScrapeUtils.Companion.getResponsiveTableFromTitle
 import com.example.sportspredictions.webscraping.WebScrapeUtils.Companion.getRowsFromFirstTable
 import it.skrape.selects.DocElement
@@ -10,25 +12,9 @@ import it.skrape.selects.html5.span
 class PassingStatsProvider {
 
     companion object {
-        fun getPassingStatsFromTable(
-            passingStatsTable: DocElement,
-            currentPlayerIndex: Int
-        ): List<String> {
-            val statsTableRows = getRowsFromFirstTable(passingStatsTable.toString())
-            val statsRow = getColumnsFromRow(statsTableRows[currentPlayerIndex])
-            val statsList: MutableList<String> = mutableListOf()
-            statsRow.forEach { statsList.add(it.text) }
-            return statsList
-        }
 
-        fun String.splitNameFromPosition(): Pair<String, String> {
-            val result = this.split(" ", limit = 3)
-            val name = result[0] + " " + result[1]
-            return Pair(name, result[2])
-        }
 
         fun getAllPassingPlayerStats(pageTables: List<DocElement>): List<PassingStats> {
-            val passingTable = getResponsiveTableFromTitle("Passing", pageTables)
             val passingTables =
                 getAllTables(getResponsiveTableFromTitle("Passing", pageTables).toString())
 
@@ -43,7 +29,8 @@ class PassingStatsProvider {
                 val nameAndPositionPair: Pair<String, String> = tableRow.span {
                     findFirst { text }
                 }.splitNameFromPosition()
-                val playerStats = getPassingStatsFromTable(playerStatsTable, index)
+
+                val playerStats = getStatsFromTable(playerStatsTable, index)
                 val player = PassingStats(
                     playerName = nameAndPositionPair.first,
                     position = nameAndPositionPair.second,
@@ -51,7 +38,7 @@ class PassingStatsProvider {
                     passCompletions = playerStats[1].toInt(),
                     passAttempts = playerStats[2].toInt(),
                     percentComplete = playerStats[3].toDouble(),
-                    passingYards = playerStats[4].toInt(),
+                    passingYards = playerStats[4].removeCommaFromNumberString().toInt(),
                     yardsPerAttempt = playerStats[5].toDouble(),
                     yardsPerGame = playerStats[6].toDouble(),
                     longestPass = playerStats[7].toInt(),
@@ -60,7 +47,6 @@ class PassingStatsProvider {
                     totalSacks = playerStats[10].toInt(),
                     sackYardsLost = playerStats[11].toInt(),
                     passerRating = playerStats[12].toDouble()
-
                 )
                 playerList.add(player)
             }
--- app/src/main/java/com/example/sportspredictions/shared/Utils.kt
+++ app/src/main/java/com/example/sportspredictions/shared/Utils.kt
@@ -1,4 +1,31 @@
 package com.example.sportspredictions.shared
 
+import com.example.sportspredictions.webscraping.WebScrapeUtils.Companion.getColumnsFromRow
+import com.example.sportspredictions.webscraping.WebScrapeUtils.Companion.getRowsFromFirstTable
+import it.skrape.selects.DocElement
+
 class Utils {
+
+    companion object{
+        fun String.splitNameFromPosition(): Pair<String, String> {
+            val result = this.split(" ", limit = 3)
+            val name = result[0] + " " + result[1]
+            return Pair(name, result[2])
+        }
+
+        fun String.removeCommaFromNumberString(): String {
+            return this.replace(",", "")
+        }
+
+        fun getStatsFromTable(
+            statsTable: DocElement,
+            currentPlayerIndex: Int
+        ): List<String> {
+            val statsTableRows = getRowsFromFirstTable(statsTable.toString())
+            val statsRow = getColumnsFromRow(statsTableRows[currentPlayerIndex])
+            val statsList: MutableList<String> = mutableListOf()
+            statsRow.forEach { statsList.add(it.text) }
+            return statsList
+        }
+    }
 }
